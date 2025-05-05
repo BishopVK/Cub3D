9e78822756997ebb3db4a2a6246df661b2a3316d
Daniel Jimenez Graindorge <danjimen@c2r11s2.42madrid.com>
2025-05-05 13:08:56 +0200

    Moved main() textures initialization to init_game_textures()

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:39:00 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/05 12:15:51 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/05/05 13:07:49 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,24 @@ static void	initialize_structs(t_map *map_s, t_map_chars *map_chars,
 	map_s->chars = map_chars; // Apuntar a la memoria de map_chars
 	map_s->floor = floor_rgb;
 	map_s->ceiling = ceiling_rgb;
+}
+
+int	init_game_textures(t_game *game, mlx_t *mlx)
+{
+	game->wall_east = mlx_load_png(game->map_s->east[1]);
+	game->wall_west = mlx_load_png(game->map_s->west[1]);
+	game->wall_south = mlx_load_png(game->map_s->south[1]);
+	game->wall_north = mlx_load_png(game->map_s->north[1]);
+	if (!game->wall_east || !game->wall_west || !game->wall_south || !game->wall_north)
+	{
+		ft_dprintf(2, "Error\n> Failed to load texture\n");
+		return (EXIT_FAILURE);
+	}
+	game->ea_img = mlx_texture_to_image(mlx, game->wall_east);
+	game->we_img = mlx_texture_to_image(mlx, game->wall_west);
+	game->so_img = mlx_texture_to_image(mlx, game->wall_south);
+	game->no_img = mlx_texture_to_image(mlx, game->wall_north);
+	return (0);
 }
 
 int	main(int argc, char *argv[])
@@ -70,13 +88,15 @@ int	main(int argc, char *argv[])
 	}
 	printf("Player x: %d, Player y: %d", map_s.player_x, map_s.player_y); // DB */
 
-	if (!(mlx = mlx_init(SCREEN_WIDTH, SCREEN_HEIGHT, "my_game", true)))
+	mlx = mlx_init(SCREEN_WIDTH, SCREEN_HEIGHT, "😈 serferna & danjimen's f**king Cub3D 😈", true);
+	if (!mlx)
 		return (EXIT_FAILURE);
 	game = game_factory(mlx, &map_s);
 	if (!game)
 		return (EXIT_FAILURE);
-
-	game->wall_east = mlx_load_png(game->map_s->east[1]);
+	if (init_game_textures(game, mlx) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	/* game->wall_east = mlx_load_png(game->map_s->east[1]);
 	game->wall_west = mlx_load_png(game->map_s->west[1]);
 	game->wall_south = mlx_load_png(game->map_s->south[1]);
 	game->wall_north = mlx_load_png(game->map_s->north[1]);
@@ -88,7 +108,7 @@ int	main(int argc, char *argv[])
 	game->ea_img = mlx_texture_to_image(mlx, game->wall_east);
 	game->we_img = mlx_texture_to_image(mlx, game->wall_west);
 	game->so_img = mlx_texture_to_image(mlx, game->wall_south);
-	game->no_img = mlx_texture_to_image(mlx, game->wall_north);
+	game->no_img = mlx_texture_to_image(mlx, game->wall_north); */
 
 	// hardcode_map(game);
 	mlx_loop_hook(mlx, ft_game_hook, game);
