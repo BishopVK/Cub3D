4acb4e64973800be96b8e905a6a57fdc67c10b3a
Zenix <sergioffdev@gmail.com>
2025-05-12 10:44:46 +0200

    Update game files, remove older mlx, and improve Makefile compiled flags

--- src/game_engine/input/mouse.c
+++ src/game_engine/input/mouse.c
@@ -11,18 +11,19 @@
 /* ************************************************************************** */
 
 #include "../../../include/cub3d.h"
+#include <math.h>
 
 // TODO: Mejorar hace cosas raras con el raton y el puntero sale en la pantalla
 // Tempblando y saltando de un lado a otro
 void	handle_mouse(t_game *game)
 {
-	int	x;
-	int	y;
+	// int	x;
+	// int	y;
 
-	mlx_get_mouse_pos(game->mlx, &x, &y);
-	if (x < SCREEN_WIDTH / 2)
-		rotate_left(game, game->frame_time * 3);
-	else if (x > SCREEN_WIDTH / 2)
-		rotate_right(game, game->frame_time * 3);
+	// mlx_get_mouse_pos(game->mlx, &x, &y);
+	// if (x < ((SCREEN_WIDTH / 2) - 10))
+	// 	rotate_left(game, game->frame_time * 3);
+	// else if (x > ((SCREEN_WIDTH / 2) + 10))
+	// 	rotate_right(game, game->frame_time * 3);
 	mlx_set_mouse_pos(game->mlx, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 }
